88a7226984973db41b2eb122344fa685edd4bc47
chaaerim <cofla411@ewhain.net>
2022-02-10 17:02:59 +0900

    delete, editing 기능 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,22 +1,53 @@
-import React from "react";
+import Tweet from "components/Tweet";
+import { dbService } from "myBase";
+import React, { useEffect } from "react";
 import { useState } from "react/cjs/react.development";
 
-const Home=()=>{
-    const[twitterclone, setTwitterclone]=useState("");
-    const onSubmit=(event)=>{
+const Home=({userObj})=>{
+    const[tweet, setTweet]=useState("");
+    const[tweets, setTweets]=useState([]);
+    
+    useEffect(()=>{
+       dbService.collection("tweets").onSnapshot((snapshot)=>{
+        const tweetArray=snapshot.docs.map(doc=>({
+            id:doc.id, 
+            ...doc.data(),
+        }));
+        setTweets(tweetArray);
+    })
+    }, []);
+    const onSubmit=async(event)=>{
         event.preventDefault();
+        await dbService.collection("tweets").add({
+           text: tweet,
+           createdAt: Date.now(),
+           creatorId: userObj.uid, 
+        });
+        setTweet("");
     };
 const onChange=(event)=>{
-    const {target:{value}, }=event;
-    setTwitterclone(value);
-}
+    const {target:{value},}=event;
+    setTweet(value);
+};
+console.log(tweets);
 return (
 <div>
-    <form>
-        <input value={twitterclone} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}/>
-        <input type="submit" value="twitter-clone"/>
+    <form onSubmit={onSubmit}>
+        <input 
+        value={tweet} 
+        onChange={onChange} 
+        type="text" 
+        placeholder="What's on your mind?" 
+        maxLength={120}
+        />
+        <input type="submit" value="Tweet"/>
     </form>
+    <div>
+        {tweets.map((tweet)=>(
+            <Tweet key={tweet.id} tweetObj={tweet} isOwner={tweet.creatorId===userObj.uid}/>
+        ))}
+    </div>
 </div>
-)
-}
+);
+};
 export default Home;
